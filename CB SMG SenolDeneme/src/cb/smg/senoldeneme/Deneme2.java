58b2ca218381a36f91bf54292238de59ba2be07d
GTUSR0405 <GTUSR0405@GTPC392.gtech.com.tr>
2024-08-07 14:41:12 +0300

    commit

--- CB SMG SenolDeneme/src/cb/smg/senoldeneme/Deneme2.java
+++ CB SMG SenolDeneme/src/cb/smg/senoldeneme/Deneme2.java
@@ -3,7 +3,7 @@ package cb.smg.senoldeneme;
 public class Deneme2 {
 
 	public static void main(String[] args) {
-		System.out.println("deneme2class");
+		System.out.println("deneme2classc");
 
 	}
 
