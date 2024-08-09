740cb36dafbe035339e7da84f32dd6489a7ee75e
GTUSR0405 <GTUSR0405@GTPC392.gtech.com.tr>
2024-08-09 14:24:25 +0300

    Merge branch 'main' of https://github.com/emrebsr/smgdeneme.git into main

--- CB SMG SenolDeneme/src/cb/smg/senoldeneme/Deneme3.java
+++ CB SMG SenolDeneme/src/cb/smg/senoldeneme/Deneme3.java
@@ -3,7 +3,7 @@ package cb.smg.senoldeneme;
 public class Deneme3 {
 
 	public static void main(String[] args) {
-		System.out.println("deneme3class 1");
+		System.out.println("deneme3clas 1");
 
 	}
 
